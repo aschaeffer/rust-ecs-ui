775cc4278972a5ba557cadd7f913f93275ead8ce
Andreas Schaeffer <hanack@nooblounge.net>
2021-02-22 18:12:09 +0100

    Refaactor FlowRuleProvider

--- src/providers/FlowRuleProvider.js
+++ src/providers/FlowRuleProvider.js
@@ -1,13 +1,14 @@
-import inherits from 'inherits';
+import inherits from 'inherits'
 
-import RuleProvider from 'diagram-js/lib/features/rules/RuleProvider';
+import RuleProvider from 'diagram-js/lib/features/rules/RuleProvider'
 
-import SocketTypes from "@/constants/SocketTypes.json"
-import InstanceTypes from "@/constants/InstanceTypes.json"
-import DataTypes from "@/constants/DataTypes.json"
+import SocketTypes from '@/constants/SocketTypes.json'
+import InstanceTypes from '@/constants/InstanceTypes.json'
+import ElementUtils from '@/utils/ElementUtils'
+import DataTypeUtils from '@/utils/DataTypeUtils'
 
 export default function FlowRuleProvider(eventBus) {
-    RuleProvider.call(this, eventBus);
+  RuleProvider.call(this, eventBus);
 }
 
 FlowRuleProvider.$inject = [ 'eventBus' ]
@@ -16,60 +17,45 @@ inherits(FlowRuleProvider, RuleProvider)
 
 
 FlowRuleProvider.prototype.init = function () {
-    this.addRule('shape.create', function (context) {
-        let target = context.target
-        let shape = context.shape
-        return target.parent === shape.target;
-    })
+  this.addRule('shape.create', function (context) {
+    let target = context.target
+    let shape = context.shape
+    return target.parent === shape.target;
+  })
 
-    // Allow outgoing properties to be the start of a connection
-    this.addRule('connection.start', function (context) {
-        let source = context.source
-        if (!Object.prototype.hasOwnProperty.call(source, 'businessObject') ||
-          typeof source.businessObject === 'undefined'
-        ) {
-            return false
-        }
-        let sourceBO = source.businessObject
-        return sourceBO.type === InstanceTypes.PROPERTY && sourceBO.socketType === SocketTypes.OUTPUT
-    })
+  // Allow outgoing properties to be the start of a connection
+  this.addRule('connection.start', function (context) {
+    let source = context.source
+    if (!Object.prototype.hasOwnProperty.call(source, 'businessObject') ||
+      typeof source.businessObject === 'undefined'
+    ) {
+      return false
+    }
+    let sourceBO = source.businessObject
+    return sourceBO.type === InstanceTypes.PROPERTY && sourceBO.socketType === SocketTypes.OUTPUT
+  })
 
-    this.addRule('connection.create', function (context) {
-        let source = context.source
-        let target = context.target
-        if (!Object.prototype.hasOwnProperty.call(source, 'businessObject') ||
-            !Object.prototype.hasOwnProperty.call(target, 'businessObject') ||
-            typeof source.businessObject === 'undefined' ||
-            typeof target.businessObject === 'undefined'
-        ) {
-            return false
-        }
-        let sourceBO = source.businessObject
-        let targetBO = target.businessObject
+  this.addRule('connection.create', function (context) {
+    let outbound = context.source
+    let inbound = context.target
+    return ElementUtils.isOutputSocket(outbound) &&
+      ElementUtils.isInputSocket(inbound) &&
+      !ElementUtils.hasIncomingConnectors(inbound) &&
+      inbound.incoming.length < 1 &&
+      DataTypeUtils.haveCompatibleDataTypes(outbound, inbound)
+  })
 
-        return sourceBO.type === InstanceTypes.PROPERTY && targetBO.type === InstanceTypes.PROPERTY &&
-            sourceBO.socketType === SocketTypes.OUTPUT && targetBO.socketType === SocketTypes.INPUT &&
-            (
-                sourceBO.dataType === DataTypes.ANY ||
-                targetBO.dataType === DataTypes.ANY ||
-                sourceBO.dataType === targetBO.dataType
-            ) &&
-            target.incoming.length < 1
-    })
+  this.addRule('shape.resize', function () {
+    return false
+  })
 
-    this.addRule('shape.resize', function () {
-        return false
-    })
+  this.addRule('shape.move', function (context) {
+    return ElementUtils.isEntity(context.shape)
+  })
 
-    this.addRule('shape.move', function (context) {
-        let shape = context.shape;
-        return shape.businessObject.type === InstanceTypes.ENTITY
-    })
-
-    // Disallow properties to be moved
-    this.addRule('elements.move', function (context) {
-        let shapes = context.shapes
-        return shapes.filter(s => s.businessObject.type === InstanceTypes.PROPERTY).length === 0
-    })
+  // Disallow properties to be moved
+  this.addRule('elements.move', function (context) {
+    return context.shapes.filter(ElementUtils.isProperty).length === 0
+  })
 
 }
