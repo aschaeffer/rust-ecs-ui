b9ea25cdbde59e5e1ee13fadd24778cdca6a26b7
Andreas Schaeffer <andreas@schaeffernet.de>
2021-03-01 18:25:54 +0100

    Add option to context pad for entities which opens the direct editing

--- src/editor/context-pad/EntityContextPadProvider.js
+++ src/editor/context-pad/EntityContextPadProvider.js
@@ -1,13 +1,15 @@
 import ElementUtils from '@/utils/ElementUtils'
 
-export default function EntityContextPadProvider(contextPad, modeling) {
+export default function EntityContextPadProvider(contextPad, modeling, directEditing) {
   this._modeling = modeling
+  this._directEditing = directEditing
   contextPad.registerProvider(this)
 }
 
 EntityContextPadProvider.$inject = [
   'contextPad',
-  'modeling'
+  'modeling',
+  'directEditing'
 ]
 
 EntityContextPadProvider.prototype.getContextPadEntries = function (element) {
@@ -16,10 +18,19 @@ EntityContextPadProvider.prototype.getContextPadEntries = function (element) {
   }
 
   let modeling = this._modeling
+  let directEditing = this._directEditing
 
   let removeElement = () => {
     modeling.removeElements([ element ])
   }
+
+  let editElement = () => {
+    directEditing.activate(element)
+  }
+
+  // let editElementDescription = () => {
+  //   directEditing.activate(element, 'description')
+  // }
 
   return {
     'delete': {
@@ -30,6 +41,22 @@ EntityContextPadProvider.prototype.getContextPadEntries = function (element) {
         click: removeElement,
         dragstart: removeElement
       }
+    },
+    'edit': {
+      group: 'edit',
+      className: 'inexor-ecs-pencil-alt',
+      title: 'Edit',
+      action: {
+        click: editElement
+      }
     }
+    // 'description': {
+    //   group: 'edit',
+    //   className: 'inexor-ecs-info',
+    //   title: 'Edit',
+    //   action: {
+    //     click: editElementDescription
+    //   }
+    // }
   }
 }
